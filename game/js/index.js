c52a31f32630f34ff884349083e7bc4a3f47b6f8
Andrew0012 <arowe@connect.kellettschool.com>
2016-06-14 15:53:49 +0800

    game upload

---
+++ game/js/index.js
@@ -0,0 +1,55 @@
+function game() {
+alert("you are an assain,master of parkour.");
+
+alert("are you ready to begin");
+
+
+var question_1=prompt("you see a weapon,should you pick it up?")
+
+if (question_1 === "yes") {
+  alert("you now have a weapon!");
+}
+
+else{
+  alert("you decide to not take a weapon. :(  (");
+}
+
+var question_2=prompt("you hear a noise, should you investigate");
+
+if (question_2 ==="yes") {
+  alert("you see the Templar leader,Jacques de Molay,Otton de Grandson and the Great Master of the Hospitallers ");
+}
+
+else{
+  alert("you run into some templars and they kill you");
+}
+
+var question_3=prompt("should you fight the leaders");
+
+if(question_3 ==="yes"){
+  alert("you kill all the templar Leaders and you are promoted to master assassin");
+}
+else{
+  alert("another assassin comes and kills the leaders ");
+}
+
+var question_4=prompt("you get the chance to retire or be an even better assassin")
+if(question_4 ===" retire"){
+  alert("you become thge most respected assassin the world has ever seen");
+}
+
+else{
+  alert("The creed decide you are not a proper assassin and they kill you when you are sleeping");
+}
+var question_5=prompt("you find the apple of eden and the sword of eden. do you keep them ")
+
+if(question_5 ===yes){
+  alert("no one can stop you and you kill everyone of your enimies");
+}
+
+else{
+  alert("you fall into a hole when you are burying them. you die"); }
+  
+alert("thanks for playing😀");
+
+}
